0455490102ee925ba89114c29a7f3a5145e640fd
Melsom <henrik.melsom@tarvalley.com>
2018-04-06 17:14:26 +0200

    initial commit

---
+++ Assets/Scripts/SlideLoader.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlideLoader : MonoBehaviour
+{
+    public RawImage current;
+    public RawImage next;
+
+    public float slideDuration = 1.0f;
+
+    FileInfo currentFile;
+
+    void Start()
+    {
+        UpdateSlides();
+        StartCoroutine(UpdateLoop());
+    }
+
+    IEnumerator UpdateLoop()
+    {
+        UpdateSlides();
+
+        yield return new WaitForSeconds(slideDuration);
+
+        StartCoroutine(UpdateLoop());
+    }
+
+    void UpdateSlides()
+    {
+        string path = Path.Combine(Application.persistentDataPath, "Slides");
+        DirectoryInfo di = new DirectoryInfo(path);
+        FileInfo[] files = di.GetFiles();
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            FileInfo f = files[i];
+            if (currentFile == null)
+            {
+                currentFile = f;
+                print(currentFile);
+                break;
+            }
+            if (currentFile.FullName == f.FullName)
+            {
+                currentFile = files[(i + 1) % files.Length];
+                break;
+            }
+        }
+
+        if (next.texture)
+        {
+            current.texture = next.texture;
+            AspectRatioFitter fitter = current.GetComponent<AspectRatioFitter>();
+            fitter.aspectRatio = (float)current.texture.width / (float)current.texture.height;
+        }
+
+        byte[] fileData = File.ReadAllBytes(currentFile.FullName);
+        Texture2D texture = new Texture2D(2, 2);
+        texture.LoadImage(fileData);
+        next.texture = texture;
+
+    }
+}
