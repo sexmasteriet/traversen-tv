36204442597609aac3c573ccf899d4ba1303a067
Melsom <henrik.melsom@tarvalley.com>
2018-04-09 18:51:31 +0200

    renamed global manager to screen manager for greater clarity

--- Assets/TraversenTV/Scripts/ScreenManager.cs
+++ Assets/TraversenTV/Scripts/ScreenManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class GlobalManager : MonoBehaviour
+public class ScreenManager : MonoBehaviour
 {
     public UnityEvent fullscreenEnter;
     public UnityEvent fullscreenExit;
